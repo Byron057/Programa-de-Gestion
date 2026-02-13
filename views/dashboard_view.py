71d9af1997b5f1176be06293cc161d031db175e0
ByronV <byronvelastegui07@gmail.com>
2026-02-13 09:03:24 -0500

    refactor: agregar AppBar y ajustar diseño en la vista del dashboard

--- views/dashboard_view.py
+++ views/dashboard_view.py
@@ -10,11 +10,26 @@ def view_dashboard(page: ft.Page):
         controls=[
             ft.Row(
             [
+            ft.AppBar(
+                title=ft.Text("Hola",color=ft.Colors.BLUE_GREY_800),
+                bgcolor=ft.Colors.GREY_200,
+                actions=[
+                ft.IconButton(
+                    ft.Icons.WB_SUNNY_OUTLINED,
+                    icon_color=ft.Colors.BLUE_GREY_800
+                ),
+                
+                ft.IconButton(ft.Icons.FILTER_3
+                
+                )]    
+            ),
+            ft.Divider(),
             ft.Container(
                 expand=1,
                 bgcolor=ft.Colors.ORANGE_300
             ),
             ft.VerticalDivider(),
+    
             ],
             width=400,
             height=800

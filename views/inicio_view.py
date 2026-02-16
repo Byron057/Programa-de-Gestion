baa9cb4348a2fac10660a05a9babd7bb5eae0bfe
ByronV <byronvelastegui07@gmail.com>
2026-02-16 10:22:34 -0500

    refactor: agregar clases de contenedor, columna, fila, icono y texto en la vista de inicio

--- views/inicio_view.py
+++ views/inicio_view.py
@@ -1,5 +1,42 @@
 import flet as ft
+class Container(ft.Container):
+    def __init__(self, width, height, color, padding, alignment, expand=False, content=None ):
+        super().__init__()
+        self.width=width
+        self.height=height
+        self.bgcolor=color
+        self.expand=expand
+        self.alignment=alignment
+        self.padding=padding
+        self.content=content
+            
+class Column(ft.Column):
+    def __init__(self, controls, expand=True):
+        super().__init__()
+        self.controls=controls
+        self.expand=expand 
+            
+class Row(ft.Row):
+    def __init__(self, controls, expand=False):
+        super().__init__()
+        self.controls=controls
+        self.expand=expand
  
+class Icon(ft.Icon):
+    def __init__(self, icon, color, size):
+        super().__init__()
+        self.icon=icon
+        self.color=color
+        self.size=size
+        
+class Text(ft.Text):
+    def __init__(self, text, size,color=None , weight=None):
+        super().__init__()
+        self.text=text
+        self.color=color
+        self.size=size
+        self.weight=weight
+
 def view_inicio(page: ft.Page):
     return ft.Container(
             expand=True,
